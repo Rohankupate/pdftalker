d4e84e277d5bb8d85c58b8972eabaa8d2a25aa0b
Rohankupate <kupaterohan@gmail.com>
2023-11-17 21:54:32 +0530

    added dilog box with react

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -34,13 +34,15 @@ const { mutate: deleteFile } =
     },
   })
   return (
-    <main className="mx-auto max-w-7xl md:p-10 h-screen">
-      <div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
-        <h1 className="mb-3 font-bold text-5xl text-gray-900">My Files</h1>
-
+    <main className='mx-auto max-w-7xl md:p-10'>
+      <div className='mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0'>
+        <h1 className='mb-3 font-bold text-5xl text-gray-900'>
+          My Files
+        </h1>
         <UploadButton />
+        
       </div>
-
+     
       {files && files.length !== 0 ? (
         <div>
           <ul className="mt-8 grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grid-cols-3'">
--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -1,39 +1,54 @@
+"use client";
+import { useState } from "react";
+import { Dialog, DialogHeader, DialogTrigger } from "./ui/dialog";
+import { Button } from "./ui/button";
+import {
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+} from "./ui/dialog";
+import Dropzone from "react-dropzone";
+import { Cloud } from "lucide-react";
+const UploadButton = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-"use client"
-import { useState } from "react"
-import { Dialog, DialogHeader, DialogTrigger } from "./ui/dialog"
-import { Button } from "./ui/button"
-import { DialogContent, DialogDescription, DialogTitle } from "@radix-ui/react-dialog"
+  const UploadDropZone = () => {
+    return (
+      <Dropzone multiple={false} onDrop={(acceptedFile=>{console.log(acceptedFile)})}>
+        {({ getRootProps, getInputProps, acceptedFiles }) => (
+          <div {...getRootProps()} className="border h-64 m-4 p-1 border-dashed border-zinc-300 rounded-lg ">
+            <div  className="flex items-center justify-centerh-full w-full ">
+              <label  htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full rounder-lg cursor-pointer bg-gray-50 hover:bg-gray-100 ">
+                <Cloud className="h-8 w-8 text-black"></Cloud>
+                <div className="flex flex-col items-center justify-center pt-5 pb-4">
+upload files here
+                </div>
+                
+              </label>
+            </div>
 
-const  UploadButton = ()=>{
-const [isOpen , setIsOpen] = useState(false)
+          </div>
+        )}
+      </Dropzone>
+    );
+  };
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={(v) => {
+        if (!v) {
+          setIsOpen(v);
+        }
+      }}
+    >
+      <DialogTrigger onClick={() => setIsOpen(true)} asChild>
+        <Button>Upload PDF</Button>
+      </DialogTrigger>
 
-return(
-
-  <Dialog
-  open={isOpen}
-  onOpenChange={(v) => {
-    if (!v) {
-      setIsOpen(v)
-    }
-  }}>
-  <DialogTrigger
-    onClick={() => setIsOpen(true)}
-    asChild>
-    <Button>Upload PDF</Button>
-  </DialogTrigger>
-
-  <DialogContent>
-    upload file 
-  </DialogContent>
-</Dialog>
+      <DialogContent>
+        <UploadDropZone></UploadDropZone>
+      </DialogContent>
+    </Dialog>
   );
-
-
-  
-   
-
-
-
-}
-export default UploadButton
+};
+export default UploadButton;
